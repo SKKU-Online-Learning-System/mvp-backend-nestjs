a7ed96449ad3eb9a3a2c74583dd7fa3382aa74e2
seah526 <seah526@naver.com>
2022-06-28 15:20:19 +0900

    transplant course api from express to nest

--- src/api/course/course.service.ts
+++ src/api/course/course.service.ts
@@ -1,36 +1,97 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { Course } from 'src/entities/course.entity';
+import { Cat1 } from 'src/entities/cat1.entity';
+import { Cat2 } from 'src/entities/cat2.entity';
+import { CourseDto } from './dto/course.dto';
 
 @Injectable()
 export class CourseService {
-	getAllCourses() {
-		return 'all courses';
+	constructor(
+		private dataSource: DataSource,
+	){}
+	async getAllCourses() {
+		const courses = await this.dataSource
+			.createQueryBuilder()
+			.select('course.title')
+			.from(Course, 'course')
+			.getMany();
+			return courses;
 	}
 
-	searchCourses() {
+	async searchCourses() {
+		
 		return 'courses filtered by query string';
 	}
 
-	getCategory1() {
-		return 'category 1';
+	async getCategory1() {
+		const cate1 = await this.dataSource
+			.createQueryBuilder()
+			.from(Cat1, 'cat1')
+			.select('cat1')
+			.getMany()
+		return cate1;
 	}
 
-	getCategory2() {
-		return 'category 2';
+	async getCategory2() {
+		const cate2 = await this.dataSource
+			.createQueryBuilder()
+			.from(Cat2, 'cat2')
+			.select('cat2')
+			.getMany()
+		return cate2;
 	}
 
-	getCourseById() {
-		return 'course by id';
+	async getCourseById(id: string ) {
+		const exist = await this.dataSource
+			.createQueryBuilder()
+			.from(Course, 'course')
+			.select('course')
+			.where("id = :id", { id })
+			.getMany()
+		return exist;
 	}
 
-	createCourse() {
-		return 'create course';
+	async createCourse({title, description, instroctorId, category1, category2, difficulty}: CourseDto) {
+		await this.dataSource 
+			.createQueryBuilder()
+			.insert()
+			.into(Course)
+			.values({
+				title, 
+				description,
+				instructor_id : instroctorId,
+				cat1_id : category1,
+				cat2_id : category2,
+				difficulty,
+			})
+			.execute();
+		return 'create course'; //
 	}
 
-	updateCourseById() {
-		return 'update course';
+	updateCourseById(id : string, {title, description, category1, category2, difficulty}: CourseDto ) {
+		const inputCourse = this.dataSource
+			.createQueryBuilder()
+			.from(Course, 'course')
+			.where("id = :id", {id})
+			.update({
+				title, 
+				description,
+				cat1_id: category1,
+				cat2_id : category2,
+				difficulty
+			})
+			.execute()
+		return 'update course'; //
 	}
 
-	deleteCourseById() {
-		return 'delete course';
+	deleteCourseById(id: string) {
+		const targetCourse = this.dataSource
+			.createQueryBuilder()
+			.from(Course, 'course')
+			.where("id = :id", {id})
+			.delete()
+			.execute()
+		//return targetCourse;
 	}
 }
