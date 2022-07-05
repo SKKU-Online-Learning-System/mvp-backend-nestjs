66dac8cc47f70fd4b578aeb26f062454c11a5a5b
cjy <paulcjy@naver.com>
2022-07-05 16:27:58 +0900

    rename mail configs 2

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,15 +1,15 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
-import { EmailConfigModule } from 'src/configs/email/email.config.module';
-import { EmailConfigService } from 'src/configs/email/email.config.service';
+import { MailConfigModule } from 'src/configs/mail/mail.config.module';
+import { MailConfigService } from 'src/configs/mail/mail.config.service';
 import { MailService } from './mail.service';
 
 @Module({
 	imports: [
 		MailerModule.forRootAsync({
-			imports: [EmailConfigModule],
-			useClass: EmailConfigService,
-			inject: [EmailConfigService],
+			imports: [MailConfigModule],
+			useClass: MailConfigService,
+			inject: [MailConfigService],
 		}),
 	],
 	providers: [MailService],
