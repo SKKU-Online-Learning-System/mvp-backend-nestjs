dc95e8d39a33fc406b2b9a5b87a6cf16a6fa2f67
seah526 <seah526@naver.com>
2022-07-11 17:02:30 +0900

    getCourseById return hashtag info

--- src/api/course/course.service.ts
+++ src/api/course/course.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource } from 'typeorm';
-import { Course } from 'src/entities/course.entity';
-import { Cat1 } from 'src/entities/cat1.entity';
-import { Cat2 } from 'src/entities/cat2.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CourseDto } from './dto/course.dto';
-
+import { CourseEntity } from 'src/entities/course.entity';
+import { Cat1Entity } from 'src/entities/cat1.entity';
+import { Cat2Entity } from 'src/entities/cat2.entity';
+import { UserEntity } from 'src/entities/user.entity';
+import { CourseHashtagEntity } from 'src/entities/course-hashtag.entity';
+import { HashtagEntity } from 'src/entities/hashtag.entity';
+import { LectureEntity } from 'src/entities/lecture.entity';
 @Injectable()
 export class CourseService {
 	constructor(
-		private dataSource: DataSource,
+		@InjectRepository(CourseEntity)
+		private courseEntity: Repository<CourseEntity>,
+		@InjectRepository(Cat1Entity)
+		private cat1Entity: Repository<Cat1Entity>,
+		@InjectRepository(Cat2Entity)
+		private cat2Entity: Repository<Cat2Entity>,
+		@InjectRepository(CourseHashtagEntity)
+		private courseHashtagEntity: Repository<CourseHashtagEntity>,
+		@InjectRepository(HashtagEntity)
+		private hashtagEntity: Repository<HashtagEntity>,
+		@InjectRepository(LectureEntity)
+		private lectureEntity: Repository<LectureEntity>
 	){}
 	async getAllCourses() {
-		const courses = await this.dataSource
-			.createQueryBuilder()
+		const courses = await this.courseEntity
+			.createQueryBuilder('course')
 			.select('course.title')
-			.from(Course, 'course')
 			.getMany();
 			return courses;
 	}
@@ -25,38 +39,49 @@ export class CourseService {
 	}
 
 	async getCategory1() {
-		const cate1 = await this.dataSource
-			.createQueryBuilder()
-			.from(Cat1, 'cat1')
+		const cate1 = await this.cat1Entity
+			.createQueryBuilder('cat1')
 			.select('cat1')
 			.getMany()
 		return cate1;
 	}
 
 	async getCategory2() {
-		const cate2 = await this.dataSource
-			.createQueryBuilder()
-			.from(Cat2, 'cat2')
+		const cate2 = await this.cat2Entity
+			.createQueryBuilder('cat2')
 			.select('cat2')
 			.getMany()
 		return cate2;
 	}
 
 	async getCourseById(id: string ) {
-		const exist = await this.dataSource
-			.createQueryBuilder()
-			.from(Course, 'course')
-			.select('course')
-			.where("id = :id", { id })
-			.getMany()
-		return exist;
+		const exist = await this.courseEntity
+			.createQueryBuilder('course')
+			.select(['course.id', 'course.title', 'course.description', 
+			'course.thumbnail', 'course.difficulty', 'course.created_at'])
+			.addSelect('user.email', 'instructor_name')
+			.addSelect('cat1.name', 'cat1_name')
+			.addSelect('cat2.name', 'cat2_name')
+			.innerJoin(Cat1Entity, 'cat1', 'cat1.id = course.cat1_id')
+			.innerJoin(Cat2Entity, 'cat2', 'cat2.id = course.cat2_id')
+			.innerJoin(UserEntity, 'user', 'user.id = course.instructor_id')
+			.where("course.id = :id", { id })
+			.getRawMany()
+
+		const hashtag = await this.courseHashtagEntity
+			.createQueryBuilder('coursehash')
+			.select('coursehash.hashtag_id')
+			.where("course_id = :id", {id})
+			.addSelect('hashtag.tag', 'tag')
+			.innerJoin(HashtagEntity, 'hashtag', 'hashtag.id = coursehash.id')
+			.getRawMany()
+		return [exist, hashtag];
 	}
 
 	async createCourse({title, description, instroctorId, category1, category2, difficulty}: CourseDto) {
-		await this.dataSource 
-			.createQueryBuilder()
+		await this.courseEntity 
+			.createQueryBuilder('course')
 			.insert()
-			.into(Course)
 			.values({
 				title, 
 				description,
@@ -70,9 +95,10 @@ export class CourseService {
 	}
 
 	updateCourseById(id : string, {title, description, category1, category2, difficulty}: CourseDto ) {
-		const inputCourse = this.dataSource
-			.createQueryBuilder()
-			.from(Course, 'course')
+		const inputCourse = this.courseEntity
+		//변수 받아오기 수정 아래로 
+		//controller에서 body로 받는 정보와 param으로 받는 정보를 한번에 
+			.createQueryBuilder('course')
 			.where("id = :id", {id})
 			.update({
 				title, 
@@ -86,10 +112,16 @@ export class CourseService {
 	}
 
 	deleteCourseById(id: string) {
-		const targetCourse = this.dataSource
-			.createQueryBuilder()
-			.from(Course, 'course')
+		//성공적으로 지워졌는지 확인 후 return 
+		const targetCourse = this.courseEntity
+			.createQueryBuilder('course')
 			.where("id = :id", {id})
+			.delete()
+			.execute()
+
+		const targetLecture = this.lectureEntity
+			.createQueryBuilder('lecture')
+			.where("course_id = :id", {id})
 			.delete()
 			.execute()
 		//return targetCourse;
