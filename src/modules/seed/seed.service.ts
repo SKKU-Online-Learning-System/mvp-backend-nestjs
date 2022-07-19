de58ac4c9d89d5b6696116ccea1fd0f78bc936ae
seah526 <seah526@naver.com>
2022-07-19 17:45:43 +0900

    test: add seed data for wishlist & learning

--- src/modules/seed/seed.service.ts
+++ src/modules/seed/seed.service.ts
@@ -6,6 +6,9 @@ import { CourseHashtagEntity } from 'src/entities/course-hashtag.entity';
 import { HashtagEntity } from 'src/entities/hashtag.entity';
 import { LectureEntity } from 'src/entities/lecture.entity';
 import { UserEntity } from 'src/entities/user.entity';
+import { AdminEntity } from 'src/entities/admin.entity';
+import { WishlistEntity } from 'src/entities/wishlist.entity';
+import { LearningEntity } from 'src/entities/learning.entity';
 import { DataSource } from 'typeorm';
 import { Category1 } from './seeds/category1.seed';
 import { Category2 } from './seeds/category2.seed';
@@ -15,7 +18,8 @@ import { Hashtag } from './seeds/hashtag.seed';
 import { Lecture } from './seeds/lecture.seed';
 import { User } from './seeds/user.seed';
 import { Admin } from './seeds/admin.seed';
-import { AdminEntity } from 'src/entities/admin.entity';
+import { Wishlist } from './seeds/wishlist.seed';
+import { Learning } from './seeds/learning.seed';
 @Injectable()
 export class SeedService {
 	constructor(private dataSource: DataSource) {}
@@ -30,6 +34,8 @@ export class SeedService {
 			{ seed: Hashtag, table: HashtagEntity },
 			{ seed: Lecture, table: LectureEntity },
 			{ seed: User, table: UserEntity },
+			{ seed: Wishlist, table: WishlistEntity},
+			{ seed: Learning, table: LearningEntity}
 		];
 
 		if (true) {
@@ -75,6 +81,12 @@ export class SeedService {
 			user: await this.dataSource
 				.getRepository(UserEntity)
 				.count(),
+			wishlist: await this.dataSource
+				.getRepository(WishlistEntity)
+				.count(),
+			learning: await this.dataSource
+				.getRepository(LearningEntity)
+				.count()
 		};
 	}
 
---
+++ src/modules/seed/seeds/learning.seed.ts
@@ -0,0 +1,93 @@
+export const Learning = [
+    {
+        id: 1,
+        userId: 1,
+        courseId: 4,
+        bookmark: true,
+    },
+    {
+        id: 2,
+        userId: 1,
+        courseId: 5,
+        bookmark: false,
+    },
+    {
+        id: 3,
+        userId: 1,
+        courseId: 9,
+        bookmark: false,
+    },
+    {
+        id: 4,
+        userId: 1,
+        courseId: 18,
+        bookmark: true,
+    },
+    {
+        id: 5,
+        userId: 1,
+        courseId: 21,
+        bookmark: false,
+    },
+    {
+        id: 6,
+        userId: 1,
+        courseId: 28,
+        bookmark: false,
+    },
+    {
+        id: 7,
+        userId: 1,
+        courseId: 33,
+        bookmark: true,
+    },
+    {
+        id: 8,
+        userId: 1,
+        courseId: 37,
+        bookmark: false,
+    },
+    {
+        id: 9,
+        userId: 1,
+        courseId: 38,
+        bookmark: false,
+    },
+    {
+        id: 10,
+        userId: 2,
+        courseId: 4,
+        bookmark: false,
+    },
+    {
+        id: 11,
+        userId: 2,
+        courseId: 9,
+        bookmark: true,
+    },
+    {
+        id: 12,
+        userId: 2,
+        courseId: 11,
+        bookmark: false,
+    },
+    {
+        id: 13,
+        userId: 2,
+        courseId: 33,
+        bookmark: false,
+    },
+    {
+        id: 14,
+        userId: 3,
+        courseId: 4,
+        bookmark: false,
+    },
+    {
+        id: 15,
+        userId: 3,
+        courseId: 40,
+        bookmark: true,
+    },
+
+]
