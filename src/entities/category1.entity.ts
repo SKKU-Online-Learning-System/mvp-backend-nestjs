8da0626ab24a8fd64287422acdd7d8d23c7acb60
cjy <paulcjy@naver.com>
2022-07-25 15:52:50 +0900

    feat: 카테고리 한 번에 가져오는 기능 추가
    - getCategories로 대분류와 소분류를 모두 조회
    - category1과 category2의 relation 설정

--- src/entities/category1.entity.ts
+++ src/entities/category1.entity.ts
@@ -1,10 +1,13 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Category2Entity } from './category2.entity';
 
 @Entity()
 class Category1 {
 	@PrimaryGeneratedColumn()
 	id: number;
 
+	@OneToMany(() => Category2Entity, (category2) => category2.category1)
+	category2s: Category2Entity[];
 	@Column({ type: 'varchar', length: 30 })
 	name: string;
 }
