6ee0f8d0676fddb638b5d0259cf69d18ca4fa949
cjy <paulcjy@naver.com>
2022-07-29 20:58:08 +0900

    feat: Swagger 파일 생성 및 변수 용어 변경
    - id를 받아오는 파라미터는 어떤 id인지 명시
    - 컨트롤러 데코레이터 순서 설정(http method가 처음, swagger가 마지막)

--- src/modules/wishlist/wishlist.controller.ts
+++ src/modules/wishlist/wishlist.controller.ts
@@ -8,29 +8,34 @@ import {
 	UseGuards,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { User } from 'src/configs/decorator/user.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { WishlistService } from './wishlist.service';
+import { ApiWishlist } from './wishlist.swagger';
 
 @ApiTags('Wishlist')
 @Controller('wishlist')
 export class WishlistController {
 	constructor(private wishlistService: WishlistService) {}
 
-	@UseGuards(JwtAuthGuard)
 	@Get()
-	getAllWishlist(@Req() req) {
-		return this.wishlistService.getAllWishlist(req.user);
+	@UseGuards(JwtAuthGuard)
+	@ApiWishlist.getAllWishlist()
+	getAllWishlist(@User() user) {
+		return this.wishlistService.getAllWishlist(user);
 	}
 
+	@Post(':courseId')
 	@UseGuards(JwtAuthGuard)
-	@Post(':id')
-	addWishlistById(@Req() req, @Param('id') id: number) {
-		return this.wishlistService.addWishlistById(req.user, id);
+	@ApiWishlist.addWishlistById()
+	addWishlistById(@User() user, @Param('courseId') id: number) {
+		return this.wishlistService.addWishlistById(user, id);
 	}
 
+	@Delete(':courseId')
 	@UseGuards(JwtAuthGuard)
-	@Delete(':id')
-	deleteWishlistById(@Req() req, @Param('id') id: number) {
-		return this.wishlistService.deleteWishlistById(req.user, id);
+	@ApiWishlist.deleteWishlistById()
+	deleteWishlistById(@User() user, @Param('courseId') id: number) {
+		return this.wishlistService.deleteWishlistById(user, id);
 	}
 }
