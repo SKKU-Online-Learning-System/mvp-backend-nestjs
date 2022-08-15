347e26aa289aa9be35a501ea1e7cdd2fac1b4385
paulcjy <86853786+paulcjy@users.noreply.github.com>
2022-08-15 16:00:23 +0900

    fix: 로그인 이메일 템플릿 이름 업데이트

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -10,7 +10,7 @@ export class MailService {
 			// from: mail.module 등록할 때 default로 입력함
 			to: userEmail,
 			subject: '온라인 명륜당 로그인',
-			template: './mail',
+			template: './login',
 			context: {
 				// ✏️ filling curly brackets with content
 				name: userEmail,
