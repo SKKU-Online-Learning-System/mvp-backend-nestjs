14799897507abf6b87eb6cb748d803b502fd2bac
Ubin108 <ybin0108@gmail.com>
2023-07-03 03:04:30 -0400

    docker ver 1.0.0

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,17 @@
-# 기본 이미지 설정
-FROM node:14-alpine
-
-# 작업 디렉터리 설정
-WORKDIR /usr/src/app
-
-# 필요한 앱 종속성 설치
+# 빌드 단계
+FROM node:14-alpine AS builder
+WORKDIR /app
 COPY package*.json ./
-COPY yarn.lock ./
-
-RUN npm install
-
-# 앱 소스 코드 복사
 COPY . .
-
-# 앱 빌드
+RUN npm install
+RUN npm i -g @nestjs/cli
+RUN npm i -g @nestjs/common
 RUN npm run build
 
-# 실행할 포트 번호 설정
-EXPOSE 3000
-
-# 컨테이너 실행 명령어 설정
-CMD ["npm", "run", "start:prod"]
+# 실행 단계
+FROM node:14-alpine
+WORKDIR /app
+ENV NODE_ENV prodution
+COPY --from=builder /app ./
+EXPOSE 4102
+CMD ["npm", "start"]
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,71 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestExpressApplication,  ExpressAdapter } from '@nestjs/platform-express';
+// import { ExpressAdapter } from '@nestjs/platform-express';
 import helmet from 'helmet';
 import * as morgan from 'morgan';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './configs/swagger/swagger.config';
 import { join } from 'path';
+const express = require('express');
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+   const server = express();
+    const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+  app.setGlobalPrefix('api');
+   swaggerConfig(app);
 
-	swaggerConfig(app);
+   // middleware
+   app.enableCors({
+	credentials: true,
+	origin: [
+	   'http://localhost:3000',
+	   'https://mrdang.cs.skku.edu',
+	   'http://mrdang.cs.skku.edu',
+	   'https://mrdang.cs.skku.edu:443',
+   ],
+	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+});
+    
+   app.use(helmet());
+   app.use(cookieParser());
+   app.use(
+      morgan('[:date[iso]] :method :url :response-time ms\t:remote-addr'),
+      //  - :remote-user\tHTTP/:http-version :res[content-length] ":referrer" ":user-agent
+   );
 
-	// middleware
-	app.enableCors({
-	 	credentials: true,
-	 	origin: 'http://localhost:3000',
-	 	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-	 });
-	 
-	app.use(helmet());
-	app.use(cookieParser());
-	app.use(
-		morgan('[:date[iso]] :method :url :response-time ms\t:remote-addr'),
-		//  - :remote-user\tHTTP/:http-version :res[content-length] ":referrer" ":user-agent
-	);
+   // static files
+   app.useStaticAssets(join(__dirname, '..', 'public'));
+   // app.useStaticAssets(join(__dirname, '..', 'public/images/banners'), {
+   //    prefix: '/bannerImage',
+   // });
+   // app.useStaticAssets(join(__dirname, '..', 'public/images/courses'), {
+   //    prefix: '/courseImage',
+   // });
+   // app.useStaticAssets(join(__dirname, '..', 'public/videos'), {
+   //    prefix: '/video',
+   // });
 
-	// static files
-	app.useStaticAssets(join(__dirname, '..', 'public'));
-	// app.useStaticAssets(join(__dirname, '..', 'public/images/banners'), {
-	// 	prefix: '/bannerImage',
-	// });
-	// app.useStaticAssets(join(__dirname, '..', 'public/images/courses'), {
-	// 	prefix: '/courseImage',
-	// });
-	// app.useStaticAssets(join(__dirname, '..', 'public/videos'), {
-	// 	prefix: '/video',
-	// });
+   // view engine
+   app.setViewEngine('pug');
+   app.setBaseViewsDir(`${__dirname}/../views`);
 
-	// view engine
-	app.setViewEngine('pug');
-	app.setBaseViewsDir(`${__dirname}/../views`);
+   app.useGlobalPipes(
+      new ValidationPipe({
+         transform: true,
+         whitelist: true,
+         forbidNonWhitelisted: true,
+         validateCustomDecorators: true,
+         disableErrorMessages: false,
+      }),
+   );
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			transform: true,
-			whitelist: true,
-			forbidNonWhitelisted: true,
-			validateCustomDecorators: true,
-			disableErrorMessages: false,
-		}),
-	);
-
-	await app.listen(4000);
+   await app.init();
+   // await app.listen(4000);
+   server.listen(4000);
 }
 bootstrap();
